2e0ac3c821d064a845ec80dbf0b74fbebb49f6d2
Vipin Sharma <67193569+Vipinsharma417@users.noreply.github.com>
2021-08-20 12:05:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+$('.count').counterUp({
+  delay:10,
+  time:3000
+});
+
+
+mybutton = document.getElementById("mybtn");
+window.onscroll = function(){
+  scrollFunction()
+};
+
+function scrollFunction(){
+  if (document.body.scrollTop>20|| document.documentElement.scroolTop>20) {
+    mybutton.style.display ="block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction(){
+  document.body.scrollTop =0;
+  document.documentElement.scrollTop = 0;
+}
